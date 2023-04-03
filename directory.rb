ddf348dd476e0220e6a3bc2d794b04c159ee3647
Julian Festing <julfesting@gmail.com>
2023-04-03 16:01:28 +0000

    Changed code so user can input students

--- directory.rb
+++ directory.rb
@@ -1,20 +1,21 @@
 # lets put all the students in an array 
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Normon Bates", cohort: :november}
-  ]
-def print_header
-  puts "The students of villain academy"
-  puts "-------------"
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
 end
 
 def print(students)
@@ -23,12 +24,33 @@ def print(students)
   end
 end
 
+def print_header
+  puts "The students of villain academy"
+  puts "-------------"
+end
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-# nothing happens until we call the methods
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
+
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november},
+#   {name: "Darth Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex DeLarge", cohort: :november},
+#   {name: "The Wicked Witch of the West", cohort: :november},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :november},
+#   {name: "The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Normon Bates", cohort: :november}
+#   ]
+
+
+# # nothing happens until we call the methods
