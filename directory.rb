c9200e206c72bc5d718abe44d66ce5683a514270
Julian Festing <julfesting@gmail.com>
2023-04-05 15:34:40 +0000

    spell challenge & refactor menu

--- directory.rb
+++ directory.rb
@@ -1,25 +1,23 @@
+@students = []
+  
 # lets put all the students in an array 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-  #return the array of students
-  students
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
@@ -33,24 +31,36 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_menu
+      puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
 
-# students = [
-#   {name: "Dr. Hannibal Lecter", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex DeLarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Krueger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Baratheon", cohort: :november},
-#   {name: "Normon Bates", cohort: :november}
-#   ]
+def show_students
+  print_header
+  print_students_list
+  print_footer(@students)
+end
 
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit # This will cause the program to terminate
+    else 
+      puts "I don't know what you meant, try again"
+  end
+end
+    
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
 
-# # nothing happens until we call the methods
+interactive_menu
